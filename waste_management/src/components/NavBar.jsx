b35bae3088803b0d161a93041990e0687f3e2a9b
Uday246r <udaychauhan246r@gamil.com>
2025-05-04 12:20:40 +0530

    route_corrected_pickupschedule

--- waste_management/src/components/NavBar.jsx
+++ waste_management/src/components/NavBar.jsx
@@ -141,7 +141,7 @@ const NavBar = () => {
                       </Link>
 
                       <Link
-                        to="/requests"
+                        to="/pickuprequest"
                         className="flex items-center px-4 py-3 hover:bg-gray-50"
                         onClick={() => setIsDropdownOpen(false)}
                       >
@@ -149,7 +149,7 @@ const NavBar = () => {
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                             d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20" />
                         </svg>
-                        <span className="text-sm text-gray-700">Requests</span>
+                        <span className="text-sm text-gray-700">Pickup Requests</span>
                       </Link>
 
                       <button
